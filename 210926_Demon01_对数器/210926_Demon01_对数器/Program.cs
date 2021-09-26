a894282f16e6f4a43bd11b39d2ad4084dcf330cb
albertzhao <szdxzhy@outlook.com>
2021-09-26 21:55:43 +0800

    Albert Algorithm

---
+++ 210926_Demon01_对数器/210926_Demon01_对数器/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _210926_Demon01_对数器
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
