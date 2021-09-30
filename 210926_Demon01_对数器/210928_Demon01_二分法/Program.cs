d05644082599d9bdb2b393517875aa8b4e7e4990
albertzhao <szdxzhy@outlook.com>
2021-09-30 16:09:39 +0800

    Add 异或运算

--- 210926_Demon01_对数器/210928_Demon01_二分法/Program.cs
+++ 210926_Demon01_对数器/210928_Demon01_二分法/Program.cs
@@ -6,7 +6,39 @@ namespace _210928_Demon01_二分法
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 1, 2, 3, 5, 5, 6, 7, 9 };
+            var isExist = BSExist(arr, 10);
+            Console.WriteLine(isExist);
+        }
+
+        /// <summary>
+        /// 二分查找：每次砍一半，O(log2N)-->O(logN)
+        /// </summary>
+        static bool BSExist(int[] arr,int value)
+        {
+            int L = 0;//左边
+            int R = arr.Length-1;//右边
+            int mid = 0;
+            while (L<R)
+            {
+                mid = L + ((R - L) >> 1);
+                if(arr[mid] == value)
+                {
+                    return true;
+                }
+                else if(arr[mid] < value)
+                {
+                    L = mid+1;
+
+                }
+                else
+                {
+                    R = mid - 1;
+                }
+            }
+            //最后一个数，边界条件
+            return arr[L] == value;
+            
         }
     }
 }
---
+++ 210926_Demon01_对数器/210930_Demon01_异或/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _210930_Demon01_异或
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            Swap(arr[1], arr[2]);
+            Console.WriteLine(arr[1]);
+            int[] arr2 = new int[] { 1, 1, 4, 4, 3, 6, 6 };
+            Console.WriteLine(FindOddNumber(arr2)); 
+
+        }
+
+        /// <summary>
+        /// 交换两个内存空间不一样的数
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void Swap(int a,int b)
+        {
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+        }
+
+        /// <summary>
+        /// 一个数组中有一种数出现了奇数次，其他数都出现了偶数次，怎么找到并打印这个数
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        static int FindOddNumber(int[] arr)
+        {
+            int eor = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                eor = eor ^ arr[i];
+            }
+            return eor;
+        }
+    }
+}
