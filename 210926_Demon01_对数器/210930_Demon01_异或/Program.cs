56fa8d36283d3914763be8c5ef8ef2833335a538
albertzhao <szdxzhy@outlook.com>
2021-10-09 17:48:16 +0800

    Add one title

--- 210926_Demon01_对数器/210930_Demon01_异或/Program.cs
+++ 210926_Demon01_对数器/210930_Demon01_异或/Program.cs
@@ -9,6 +9,14 @@ namespace _210930_Demon01_异或
             int test = 7;
             System.Console.WriteLine((~test)+1);
             System.Console.WriteLine("================");
+            int[] arrOne = new int[] { 4, 6, 6, 8, 8, 10, 10,10 };
+            FindTwoNumberByOrder(arrOne);
+            System.Console.WriteLine("================");
+            int[] arrTwo = new int[] { 4, 4,4, 6, 6, 10, 10, 10,11,11,11 };
+            int kValue = FindOnlyKOtherM(arrTwo, 2, 3);
+            Console.WriteLine($"那个出现K次的数为{kValue}");
+            System.Console.WriteLine("================");
+
             int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
             Swap(arr[1], arr[2]);
             Console.WriteLine(arr[1]);
@@ -44,6 +52,69 @@ namespace _210930_Demon01_异或
                 eor = eor ^ arr[i];
             }
             return eor;
+        }
+
+        /// <summary>
+        /// 有两种数，出现奇数次
+        /// </summary>
+        /// <param name="arr"></param>
+        static void FindTwoNumberByOrder(int[] arr)
+        {
+            int eor = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                eor = eor ^ arr[i];              
+            }
+            int rightOne = eor & (~eor + 1);//提取出最右的1
+            int eorRight = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if((rightOne&arr[i]) != 0)
+                {
+                    eorRight = eorRight ^ arr[i];
+                }
+            }
+            Console.WriteLine($"其中的一个数为{eorRight}");
+            Console.WriteLine("======================");
+            Console.WriteLine($"另一个数为{eor^eorRight}");
+        }
+
+
+        /// <summary>
+        /// 请保证arr中，只有一种数出现了K次，其他数都出现了M次（K<M）
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        static int FindOnlyKOtherM(int[] arr, int k, int m)
+        {
+            int[] arr32T = new int[32];
+            int kValue = 0;
+            //arr32T[0] 0位置出现了几个
+            //arr32T[1] 1位置出现了几个
+            //arr32T[2] 2位置出现了几个
+            foreach (var item in arr)
+            {
+                for (int i = 0; i < 32; i++)
+                {
+                    //通过右移，将每一位出现1的累加到记录数组中
+                    if (((item >> i) & 1) != 0)
+                    {
+                        arr32T[i]++;
+                    }
+                }
+            }
+            //遍历数组，如果某一位出现的次数不是M的整数倍，则必定是K那个数存在了1
+            for (int i = 0; i < 32; i++)
+            {
+                if ((arr32T[i] % m) != 0)
+                {
+                    //kValue += (int)Math.Pow(2, i);
+                    //1左移i位，然后或进去
+                    kValue |= (1 << i);
+                }
+            }
+            return kValue;
+
         }
     }
 }
